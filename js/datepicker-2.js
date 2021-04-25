d9e1bd872fa269bf8d69f3a9d56bc4d6a126d941
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2021-04-25 14:21:08 +0200

    [bug: datepicker go forward and back]

--- js/datepicker-2.js
+++ js/datepicker-2.js
@@ -10,6 +10,16 @@ const lookup = {
     5: 31,
 };
 
+const daynames = {
+    0: "Zondag",
+    1: "Maandag",
+    2: "Dinsdag",
+    3: "Woensdag",
+    4: "Donderdag",
+    5: "Vrijdag",
+    6: "Zaterdag",
+};
+
 const focusElements = [];
 
 const monthNames = [
@@ -39,6 +49,8 @@ const hours = today.getHours() + 1;
 const time = `${hours}:${today.getMinutes()}`;
 
 let day = today.getDate();
+let currentdayNumber = today.getDay();
+let test = currentdayNumber;
 
 const year = today.getFullYear();
 
@@ -83,7 +95,17 @@ const fillIn = (container, year, month, day) => {
     node.target = "_blank";
     node.className = `datum`;
     node.id = className;
-    node.innerText = day;
+
+    for (const [key, value] of Object.entries(daynames)) {
+        if (parseInt(key) === test) {
+            node.innerText = `${day} ${value}`;
+        }
+    }
+    test++;
+
+    if (test === 7) {
+        test = 0;
+    }
     container.appendChild(node);
     focusElements.push(`${className}`);
     className++;
@@ -93,20 +115,20 @@ window.addEventListener("keydown", function(event) {
     const keycode = event.keyCode;
     const length = focusElements.length;
 
-    if (begin == length) {
-        begin = 0;
-    }
-
     if (keycode === 74) {
+        if (begin == length) {
+            begin = 0;
+        }
         document.getElementById(`${focusElements[begin]}`).focus();
         begin++;
     }
 
     if (keycode === 72) {
+        console.log(begin, length);
         begin = begin - 1;
         if (begin + 1 === 0 || begin < 0) {
-            begin = length;
-            return;
+            begin = 19;
+            return begin;
         }
         document.getElementById(`${focusElements[begin]}`).focus();
     }
