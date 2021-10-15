34b4948d58cbc64bf8835d719aa1abfaddd72c43
Chubby Racoon <33514521+RowinRuizendaal@users.noreply.github.com>
2021-10-15 09:56:45 +0200

    Update datepicker-2.js

--- js/datepicker-2.js
+++ js/datepicker-2.js
@@ -8,7 +8,13 @@ const maand2 = document.querySelector(".maand-2");
 const lookup = {
     4: 30,
     5: 31,
-    6: 30
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
 };
 
 const daynames = {
